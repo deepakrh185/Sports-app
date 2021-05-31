086326848097de752f481a787760f2464f0ff61c
Deepak <deepsrh1801@gmail.com>
2021-05-31 13:19:34 +0530

    added features in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
-import { SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline";
+import {
+  SearchIcon,
+  ShoppingBagIcon,
+  BriefcaseIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 import Image from "next/image";
 
 function Header() {
   return (
     <header>
-      <div className="flex items-center bg-green-600 flex-grow ">
-        <div className="md:ml-52 lg:ml-52 "></div>
-        <div className="flex items-center flex-grow sm:flex-grow-0  mr-2">
+      <div className="flex items-center bg-green-600  ">
+        <div className="md:ml-2 lg:ml-52 "></div>
+        <div className="flex items-center flex-grow sm:flex-grow-0  mr-2 ">
           <Image
             src="/dew.png"
             width={100}
@@ -22,15 +27,20 @@ function Header() {
           <SearchIcon className="h-12 p-4" />
         </div>
         <div className="text-white flex items-center text-sm space-x-6 mx-6 whitespace-nowrap cursor-pointer ">
-          <div>
-            <p className="link font-bold">Sign In</p>
+          <div className="link flex">
+            <UserIcon className="h-6" />
+            <p className=" link font-bold ml-2 sm:inline mt-1">Sign In</p>
           </div>
-          <div className="link">
-            <p className="font-bold md:text-sm">Your Orders</p>
+          <div className="link flex">
+            <BriefcaseIcon className="h-6" />
+            <p className="link font-bold ml-2 sm:inline mt-1 ">Your Orders</p>
           </div>
           <div className="relative flex items-center flex-shrink">
-            <ShoppingCartIcon className="h-8" />
-            <p className="link font-bold md:text-sm hidden sm:inline mt-2 md:mr-2 lg:mr-52 sm:mr-2 flex-shrink">
+            <span className="absolute top-0 right-0 left-4 md:right-10 sm:right-10 h-4 w-4 bg-green-300 text-center rounded-full text-black font-bold  text-xs">
+              0
+            </span>
+            <ShoppingBagIcon className="h-6" />
+            <p className="link font-extrabold  ml-2  mt-1 md:mr-2 lg:mr-52 sm:mr-2 ">
               Cart
             </p>
           </div>
