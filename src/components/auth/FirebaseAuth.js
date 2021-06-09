94021d3c31f812d04ef81ccc76e30e78b71dad52
Deepak <deepsrh1801@gmail.com>
2021-06-10 00:14:12 +0530

    added fb login

--- src/components/auth/FirebaseAuth.js
+++ src/components/auth/FirebaseAuth.js
@@ -19,7 +19,7 @@ const firebaseAuthConfig = {
     },
     // add additional auth flows below
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
   ],
   signInSuccessUrl: "/",
